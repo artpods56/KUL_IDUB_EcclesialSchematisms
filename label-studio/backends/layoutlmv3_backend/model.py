bff87e87580866fc024bfebb2e881710db6e6522
Artur Podsiadły <artpods56@gmail.com>
2025-04-28 19:04:45 +0200

    chore: changed the used envirnoment variable for boto3 client

--- label-studio/backends/layoutlmv3_backend/model.py
+++ label-studio/backends/layoutlmv3_backend/model.py
@@ -70,7 +70,7 @@ class LayoutLMv3Backend(LabelStudioMLBase):
 
         self._lazy_init()
 
-        endpoint_url = os.getenv("S3_ENDPOINT_URL")
+        endpoint_url = os.getenv("AWS_S3_ENDPOINT_URL")
         aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
         aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
         region_name = os.getenv("AWS_REGION", "us-east-1")
