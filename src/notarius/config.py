3d35f4cd15a40a6d862380c7590329052d772dcc
Artur Podsiadły <artpods56@gmail.com>
2026-01-11 15:43:33 +0100

    chore(config): update application and dataset configurations
    - update main config
    - modify base HuggingFace dataset config

--- src/notarius/config.py
+++ src/notarius/config.py
@@ -5,13 +5,18 @@ from pydantic_settings import SettingsConfigDict, BaseSettings
 
 from notarius.shared.constants import REPOSITORY_ROOT
 
+
 class StorageConfig(BaseSettings):
     model_config = SettingsConfigDict(env_file=REPOSITORY_ROOT / ".env", extra="allow")
     storage_root: Path
 
 
+class DagsterConfig(BaseSettings):
+    port: int = 3000
+    host: str = "localhost"
 
-class AppConfig(StorageConfig):
+
+class AppConfig(StorageConfig, DagsterConfig):
     model_config = SettingsConfigDict(env_file=REPOSITORY_ROOT / ".env", extra="allow")
 
     log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
